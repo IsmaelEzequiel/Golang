f3e83eee3986874f511b2a1e6268bd5adbb85e65
ismaelezequiel <kell.trollhammaren@hotmail.com>
2024-05-17 03:09:24 -0300

    adds campaign domain and tdd

--- internal/domain/compaign/campaign.go
+++ internal/domain/compaign/campaign.go
@@ -1,6 +1,11 @@
 package compaign
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/rs/xid"
+)
 
 type Contacts struct {
 	Email string
@@ -14,5 +19,27 @@ type Campaign struct {
 	Contacts  []Contacts
 }
 
-func NewCompaign(name string, content string) {
+func NewCompaign(name string, content string, emails []string) (*Campaign, error) {
+
+	if name == "" {
+		return nil, errors.New("name is required")
+	} else if content == "" {
+		return nil, errors.New("content is required")
+	} else if len(emails) == 0 {
+		return nil, errors.New("emails is required")
+	}
+
+	contacts := make([]Contacts, len(emails))
+
+	for index, email := range emails {
+		contacts[index].Email = email
+	}
+
+	return &Campaign{
+		ID:        xid.New().String(),
+		Name:      name,
+		CreatedAt: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}, nil
 }
